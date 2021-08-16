6aec76c75af3a0fabc9c8087fb5de0d71b9daf34
mitzigisselle <72286934+mitzigisselle@users.noreply.github.com>
2021-08-16 10:39:50 -0500

    agregado de base de datos

---
+++ bd/helpdesk.sql
@@ -0,0 +1,289 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-08-2021 a las 17:02:12
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `helpdesk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_asignacion`
+--
+
+CREATE TABLE `t_asignacion` (
+  `id_asignacion` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_equipo` int(11) NOT NULL,
+  `marca` varchar(245) DEFAULT NULL,
+  `modelo` varchar(245) DEFAULT NULL,
+  `color` varchar(245) DEFAULT NULL,
+  `descripcion` varchar(245) DEFAULT NULL,
+  `memoria` varchar(245) DEFAULT NULL,
+  `disco_duro` varchar(245) DEFAULT NULL,
+  `procesador` varchar(245) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_asignacion`
+--
+
+INSERT INTO `t_asignacion` (`id_asignacion`, `id_persona`, `id_equipo`, `marca`, `modelo`, `color`, `descripcion`, `memoria`, `disco_duro`, `procesador`) VALUES
+(3, 1, 1, 'DELL', 'Optiplex', 'Gris', 'Es una pc slim y se puede poner en monitor sobre ella', '4 GB', '1 TB', 'Core i3'),
+(4, 1, 2, 'Acer', 'AET21', 'Rojo', 'El teclado esta en ingles y no tiene ralladuras la maquina', '8 GB', '120 SSD', 'Core i5 10500');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_cat_equipo`
+--
+
+CREATE TABLE `t_cat_equipo` (
+  `id_equipo` int(11) NOT NULL,
+  `nombre` varchar(245) NOT NULL,
+  `descripcion` varchar(245) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_cat_equipo`
+--
+
+INSERT INTO `t_cat_equipo` (`id_equipo`, `nombre`, `descripcion`) VALUES
+(1, 'PC', 'fas fa-desktop'),
+(2, 'Laptop', 'fas fa-laptop'),
+(3, 'Mouse', NULL),
+(4, 'Teclado', NULL),
+(5, 'Monitor', NULL),
+(6, 'Bocinas', NULL),
+(7, 'Microfono', NULL),
+(8, 'Proyector', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_cat_roles`
+--
+
+CREATE TABLE `t_cat_roles` (
+  `id_rol` int(11) NOT NULL,
+  `nombre` varchar(245) NOT NULL,
+  `descripcion` varchar(245) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_cat_roles`
+--
+
+INSERT INTO `t_cat_roles` (`id_rol`, `nombre`, `descripcion`) VALUES
+(1, 'cliente', 'Es un cliente'),
+(2, 'admin', 'Es Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_persona`
+--
+
+CREATE TABLE `t_persona` (
+  `id_persona` int(11) NOT NULL,
+  `paterno` varchar(245) NOT NULL,
+  `materno` varchar(245) DEFAULT NULL,
+  `nombre` varchar(245) NOT NULL,
+  `fecha_nacimiento` varchar(245) NOT NULL,
+  `sexo` varchar(2) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  `correo` varchar(245) DEFAULT NULL,
+  `fechaInsert` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_persona`
+--
+
+INSERT INTO `t_persona` (`id_persona`, `paterno`, `materno`, `nombre`, `fecha_nacimiento`, `sexo`, `telefono`, `correo`, `fechaInsert`) VALUES
+(1, 'help', 'desk', 'demo', '2021-08-09', 'M', '56895623', 'helpdesk@gmail.com', '2021-08-09 14:18:27'),
+(2, 'lopez', 'martinez', 'juan', '2000-01-10', 'M', '88', 'correo@gmail.com', '2021-08-10 13:59:19'),
+(3, 'lopez', 'martinez', 'juan', '2000-01-11', 'M', '8885', 'correo@gmail.com', '2021-08-10 14:00:37');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_reportes`
+--
+
+CREATE TABLE `t_reportes` (
+  `id_reporte` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_equipo` int(11) NOT NULL,
+  `id_usuario_tecnico` int(11) DEFAULT NULL,
+  `descripcion_problema` text NOT NULL,
+  `solucion_problema` text DEFAULT NULL,
+  `estatus` int(11) NOT NULL,
+  `fecha` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_reportes`
+--
+
+INSERT INTO `t_reportes` (`id_reporte`, `id_usuario`, `id_equipo`, `id_usuario_tecnico`, `descripcion_problema`, `solucion_problema`, `estatus`, `fecha`) VALUES
+(1, 2, 1, NULL, 'mndkvnzkv', NULL, 0, '2021-08-16');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_usuarios`
+--
+
+CREATE TABLE `t_usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `usuario` varchar(245) NOT NULL,
+  `password` varchar(245) NOT NULL,
+  `ubicacion` text DEFAULT NULL,
+  `activo` int(11) NOT NULL DEFAULT 1,
+  `fecha_insert` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `t_usuarios`
+--
+
+INSERT INTO `t_usuarios` (`id_usuario`, `id_rol`, `id_persona`, `usuario`, `password`, `ubicacion`, `activo`, `fecha_insert`) VALUES
+(1, 2, 1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Modulo 1', 1, NULL),
+(2, 1, 1, 'cliente', 'd94019fd760a71edf11844bb5c601a4de95aacaf', 'Modulo 1', 1, NULL),
+(3, 1, 3, 'usuario1', 'ada6d34bca926b40be00893cabc0aeae138ea2a0', 'esta en el cubÃ­culo 25', 1, NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `t_asignacion`
+--
+ALTER TABLE `t_asignacion`
+  ADD PRIMARY KEY (`id_asignacion`),
+  ADD KEY `fkPersona_idx` (`id_persona`),
+  ADD KEY `fkPersonaAsignacion_idx` (`id_persona`),
+  ADD KEY `fkequipoAsignacion_idx` (`id_equipo`);
+
+--
+-- Indices de la tabla `t_cat_equipo`
+--
+ALTER TABLE `t_cat_equipo`
+  ADD PRIMARY KEY (`id_equipo`);
+
+--
+-- Indices de la tabla `t_cat_roles`
+--
+ALTER TABLE `t_cat_roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `t_persona`
+--
+ALTER TABLE `t_persona`
+  ADD PRIMARY KEY (`id_persona`);
+
+--
+-- Indices de la tabla `t_reportes`
+--
+ALTER TABLE `t_reportes`
+  ADD PRIMARY KEY (`id_reporte`),
+  ADD KEY `fkUsuarioReporte_idx` (`id_usuario`),
+  ADD KEY `fkEquipoReporte_idx` (`id_equipo`);
+
+--
+-- Indices de la tabla `t_usuarios`
+--
+ALTER TABLE `t_usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `fkPersona_idx` (`id_persona`),
+  ADD KEY `fkRoles_idx` (`id_rol`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `t_asignacion`
+--
+ALTER TABLE `t_asignacion`
+  MODIFY `id_asignacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `t_cat_equipo`
+--
+ALTER TABLE `t_cat_equipo`
+  MODIFY `id_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `t_cat_roles`
+--
+ALTER TABLE `t_cat_roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `t_persona`
+--
+ALTER TABLE `t_persona`
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `t_reportes`
+--
+ALTER TABLE `t_reportes`
+  MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `t_usuarios`
+--
+ALTER TABLE `t_usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `t_asignacion`
+--
+ALTER TABLE `t_asignacion`
+  ADD CONSTRAINT `fkPersonaAsignacion` FOREIGN KEY (`id_persona`) REFERENCES `t_persona` (`id_persona`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fkequipoAsignacion` FOREIGN KEY (`id_equipo`) REFERENCES `t_cat_equipo` (`id_equipo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `t_reportes`
+--
+ALTER TABLE `t_reportes`
+  ADD CONSTRAINT `fkEquipoReporte` FOREIGN KEY (`id_equipo`) REFERENCES `t_cat_equipo` (`id_equipo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fkUsuarioReporte` FOREIGN KEY (`id_usuario`) REFERENCES `t_usuarios` (`id_usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `t_usuarios`
+--
+ALTER TABLE `t_usuarios`
+  ADD CONSTRAINT `fkPersona` FOREIGN KEY (`id_persona`) REFERENCES `t_persona` (`id_persona`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fkRoles` FOREIGN KEY (`id_rol`) REFERENCES `t_cat_roles` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
